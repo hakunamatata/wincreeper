937e195f8e43fe468b514b9745e14290868d9957
hakunamatata <xymbtc@163.com>
2019-04-11 17:12:39 +0800

    update

--- AustraliaVote/index.js
+++ AustraliaVote/index.js
@@ -4,18 +4,79 @@ Vue.prototype.wx = weui;
 
 /* API */
 const server = 'http://localhost:56988/';
-var get_topic = (topicId, callback, error) => {
+/**
+ * 获取投票主题的信息
+ * @param {String} topicId 主题ID
+ * @param {Function} callback 接口调用成功
+ * @param {Function} error 接口调用失败
+ */
+function get_topic(topicId, success, error) {
     axios.get(server + 'vote/topic/' + topicId)
         .then(res => {
             if (res.status == 200) {
-                if (callback) callback(res.data);
+                if (success) success(res.data);
             }
             else {
                 if (error) error(res);
             }
-        });
+        })
+        .catch(error);
+};
 
+/**
+ * 对主题进行投票
+ * @param {String} topicId 主题ID
+ * @param {Object} data 投票数据
+ * {
+ *   uid: String 微信用户的openid,
+ *   subs: Array 主题中所有投票题目
+ *   [{
+ *      id: String 题目编号
+ *      ops: Array 题目中的选项
+ *      [{
+ *        id: String 选项编号
+ *        order:Number 选择顺序
+ *      }]
+ *   }]
+ * }
+ * @param {Function} success 接口调用成功回调
+ * @param {Function} error 接口调用失败回调
+ */
+function vote_topic(topicId, data, success, error) {
+    axios.post(server + 'vote/topic/' + topicId, data)
+        .then(res => {
+            if (res.status == 200) {
+                if (success) success(res.data);
+            }
+            else {
+                if (error) error(res);
+            }
+        })
+        .catch(error);
+
+};
+
+/**
+ * 喜欢某一个主题
+ * @param {String} topicId 主题编号
+ * @param {Function} success 接口调用成功回调
+ * @param {Function} error 接口调用失败回调
+ * @return {Number} 返回当前主题被喜欢的数量
+ */
+function like_topic(topicId, success, error) {
+    axios.post(server + 'vote/topic/like', { tid: topicId })
+        .then(res => {
+            if (res.status == 200) {
+                if (success) success(res.data);
+            }
+            else {
+                if (error) error(res);
+            }
+        })
+        .catch(error);
 }
+
+
 new Vue({
     el: '#app',
     data() {
@@ -27,5 +88,6 @@ new Vue({
             res => {
                 console.log(res);
             });
+
     }
 });
