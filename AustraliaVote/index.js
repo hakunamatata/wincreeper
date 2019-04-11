b9538b8e66b4d92d2359f99da5321e875598a3b8
hakunamatata <xymbtc@163.com>
2019-04-11 16:40:34 +0800

    update

--- AustraliaVote/index.js
+++ AustraliaVote/index.js
@@ -3,7 +3,15 @@ Vue.prototype.$http = axios;
 Vue.prototype.wx = weui;
 
 /* API */
-var get_topic = (topicId, callback) => {
+const server = 'http://localhost:56988/';
+var get_topic = (topicId, callback, error) => {
+    axios.get(server + 'vote/topic/' + topicId)
+        .then(res => {
+            if (res.status == 200)
+                callback(res.data);
+            else
+                error(res);
+        });
 
 }
 new Vue({
@@ -12,4 +20,7 @@ new Vue({
         return {
         }
     },
+    mounted() {
+        get_topic('2166554f-14f9-43ad-ae2b-cd9f59e065bf');
+    }
 });
